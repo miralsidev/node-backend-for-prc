c468aac6956cfb6367a4938132cd1f7bc4956e82
miralsidev <miralsidev@gmail.com>
2024-05-20 01:00:14 +0530

    addcode

--- controller/User.js
+++ controller/User.js
@@ -1,51 +1,48 @@
 const { User } = require("../models/User");
 
-const bcrypt = require("bcrypt");
-const { UserServices } = require("../services/UserService");
-
-
-const nodemailer = require("nodemailer");
 const addUser = async (req, res) => {
   console.log("hello");
-  const { fname, lname, addres, password, email } = req.body;
-  const hashpassword = await bcrypt.hash(password, 10);
-  const findName = await User.findOne({ fname: fname });
-  console.log("find name =", findName);
-  if (findName) {
-    res.json({ message: "fname already exist" });
-  } else {
+  const { fname, lname, email } = req.body;
+  // const hashpassword = await bcrypt.hash(password, 10);
+  // const findName = await User.findOne({ fname: fname });
+  // console.log("find name =", findName);
+  // if (findName) {
+  //   res.json({ message: "fname already exist" });
+  // } else {
     const data = new User({
       fname: fname,
       lname: lname,
-      addres: addres,
-      password: hashpassword,
+      // addres: addres,
+      // password: password,
       email: email,
     });
-    const mailTransporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-    const mailoption = {
-      form: process.env.EMAIL_FORM,
-      to: email,
-      subject: "reg details",
-      text: "registartion succsfully",
-    };
-    mailTransporter.sendMail(mailoption, (error, info) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("email sent = ", info.response);
-      }
-    });
-    UserServices.create(data).then((data) => {
-      res.json({ message: "succsessfully..!!" });
-    });
+    // const mailTransporter = nodemailer.createTransport({
+    //   host: process.env.EMAIL_HOST,
+    //   port: process.env.EMAIL_PORT,
+    //   secure: false,
+    //   auth: {
+    //     user: process.env.EMAIL_USER,
+    //     pass: process.env.EMAIL_PASS,
+    //   },
+    // });
+    // const mailoption = {
+    //   form: process.env.EMAIL_FORM,
+    //   to: email,
+    //   subject: "reg details",
+    //   text: "registartion succsfully",
+    // };
+    // mailTransporter.sendMail(mailoption, (error, info) => {
+    //   if (error) {
+    //     console.log(error);
+    //   } else {
+    //     console.log("email sent = ", info.response);
+    //   }
+    // });
+    await data.save();
+    res.json({ message: "succsessfully..!!" });
+    // UserServices.create(data).then((data) => {
+    //   res.json({ message: "succsessfully..!!" });
+    // });
   }
-};
+// };
 module.exports = {addUser}
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const { mongoose } = require("mongoose");
+const cors = require('cors');
 require("dotenv").config();
-// const {router} = require("./routes/User")
+const {router} = require("./routes/User")
 db_url = process.env.db_url;
 port = process.env.port;
 const express = require("express");
 const app = express();
+app.use(cors());
+app.use(express.json());
 const connectdb = async (db_url) => {
   const db_option = {
     dbname: process.env.dbname,
@@ -14,7 +17,7 @@ const connectdb = async (db_url) => {
     .then(() => {
       console.log("connection successfully..!!");
       app.use(express.json());
-    //   app.use("/api",router)
+      app.use("/api",router)
       app.listen(port, () =>
         console.log(`Example app listening on port ${port}!`)
       );

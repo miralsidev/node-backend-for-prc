76e333df616871617bbcd04eab9b208228271b83
miralsidev <miralsidev@gmail.com>
2024-05-20 23:25:30 +0530

    addddd

--- controller/User.js
+++ controller/User.js
@@ -1,17 +1,34 @@
 const { User } = require("../models/User");
 const addUser = async (req, res) => {
-  console.log("hello");
-  const { fname, lname, email } = req.body;
-    const data = new User({
-      fname: fname,
-      lname: lname,
-      email: email,
-    });
-    await data.save();
-    res.json({ message: "succsessfully..!!" });
+  try {
+    console.log("hello");
+    const { fname, lname, email, password } = req.body;
+    if (fname && lname && email && password) {
+      const data = new User({
+        fname: fname,
+        lname: lname,
+        email: email,
+        password: password,
+      });
+      await data.save();
+      res.json({ message: "succsessfully..!!" });
+    } else {
+      return res.json({ status: 400, message: "all filed are required" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.json({ status: 500, message: "internal server error " ,error});
+  
   }
-  const getUser = async(req,res)=>{
+};
+
+const getUser = async (req, res) => {
+  try {
     const data = await User.find();
-    return res.json({message:"display all data ",data:data})
+    return res.json({ message: "display all data ", data: data });
+  } catch (error) {
+    
   }
-module.exports = {addUser,getUser}
+
+};
+module.exports = { addUser, getUser };

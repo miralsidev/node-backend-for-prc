28e450986aecf015b6836d9adaf384b114df47ae
miralsidev <miralsidev@gmail.com>
2024-05-21 22:49:11 +0530

    addcode

--- routes/User.js
+++ routes/User.js
@@ -3,6 +3,6 @@ const userController = require("../controller/User");
 
 router.post("/addUser", userController.addUser)
 router.get("/displayUser", userController.getUser)
-router.get("/login",userController.login)
+router.post("/login",userController.login)
 
 module.exports = router

dd17a9cd31a294ea6d1f95ca47dd019c2d733d24
miralsidev <miralsidev@gmail.com>
2024-05-21 23:43:39 +0530

    addcode

---
+++ Helper/mail.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const nodemailer = require('nodemailer')
+const mail = (email,subject,text) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_POST,
+        secure: false,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
+      const mailOptions = {
+        from: process.env.EMAIL_FROM,
+        to: email,
+        subject: subject,
+        text: text,
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("email sent = " + info.response);
+        }
+      });
+}
+module.exports={mail}
--- controller/User.js
+++ controller/User.js
@@ -1,12 +1,13 @@
 const { User } = require("../models/User");
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { mail } = require("../Helper/mail");
+require("dotenv").config();
 const addUser = async (req, res) => {
   try {
     console.log("hello");
     const { fname, lname, email, password } = req.body;
-    const hashpassword = await bcrypt.hash(password, 10)
+    const hashpassword = await bcrypt.hash(password, 10);
     console.log("hashpassword=", hashpassword);
     if (fname && lname && email && password) {
       const data = new User({
@@ -23,7 +24,6 @@ const addUser = async (req, res) => {
   } catch (error) {
     console.error(error);
     return res.json({ status: 500, message: "internal server error ", error });
-
   }
 };
 
@@ -41,31 +41,30 @@ const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    if (email && password){
-
-      
-        const user = await User.findOne({ email });
+    if (email && password) {
+      const user = await User.findOne({ email });
       if (!user) {
         return res.status(400).json({ message: "Invalid email or password" });
       }
-  
+
       const isMatch = await bcrypt.compare(password, user.password);
-  
+
       if (!isMatch) {
         return res.status(400).json({ message: "Invalid email or password" });
       }
       let jwtSecretKey = process.env.secret_key;
-      const token = jwt.sign({ userId: user._id },jwtSecretKey);
-  
+      const token = jwt.sign({ userId: user._id }, jwtSecretKey);
+      subject = "Login succesfully..!!";
+      (text =
+        "Welcome to SpeedyWheels Rentals! We are thrilled to have you as a member of our community. "),
+        mail(email, subject, text);
       res.json({ message: "Login successful", token });
-
-    }
-    else{
-      return res.json({ message: "all filed are required ..!" })
+    } else {
+      return res.json({ message: "all filed are required ..!" });
     }
   } catch (error) {
     console.error(error);
     return res.json({ status: 500, message: "internal server error ", error });
   }
-}
+};
 module.exports = { addUser, getUser, login };
